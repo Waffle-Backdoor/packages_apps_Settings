2adc9e56470ebb5823a5a67856a49f0cc025d020
Anay Wadhera <anay1018@gmail.com>
2022-10-30 18:25:57 -0400

    Settings: Create face confirmation activity
    Change-Id: Id245c958a3f32e36620240fab6b9b4961bee35d5
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/google/android/settings/biometrics/face/FaceEnrollConfirmation.java
+++ src/com/google/android/settings/biometrics/face/FaceEnrollConfirmation.java
@@ -11,7 +11,8 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
-import com.android.settings.R;
+import androidx.window.R;
+
 
 import com.google.android.setupcompat.template.FooterBarMixin;
 import com.google.android.setupcompat.template.FooterButton;
@@ -41,7 +42,7 @@ public class FaceEnrollConfirmation extends FragmentActivity {
             this.mUserId = bundle.getInt("android.intent.extra.USER_ID", this.mUserId);
         }
         setHeaderText(R.string.security_settings_face_enroll_finish_title);
-        getLayout().setDescriptionText(R.string.security_settings_face_enroll_finish_description_with_bp);
+        getLayout().setDescriptionText(R.string.security_settings_face_enroll_finish_description);
         FooterBarMixin footerBarMixin = (FooterBarMixin) getLayout().getMixin(FooterBarMixin.class);
         this.mFooterBarMixin = footerBarMixin;
         footerBarMixin.setPrimaryButton(
@@ -55,7 +56,7 @@ public class FaceEnrollConfirmation extends FragmentActivity {
                                     }
                                 })
                         .setButtonType(5)
-                        .setTheme(com.google.android.setupdesign.R.style.SudGlifButton_Primary)
+                        .setTheme(R.style.SudGlifButton_Primary)
                         .build());
     }
 
